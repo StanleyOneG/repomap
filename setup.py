74d87289be7cf7f36fc84ef0ad9ed2e4b3ed4b75
Stanislav <goncharuk.sm@yandex.ru>
2025-01-15 17:55:50 +0300

    fix: setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="repomap",
     version="0.1.0",
-    author="Stanislav",
+    author="Stanislav Goncharuk",
     description="Repository map generator for GitLab repositories",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,17 +20,16 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=[
-        "requests>=2.25.0",
-        "tree-sitter-languages>=1.5.0",
-        "python-dotenv>=0.19.0",
+        "python-gitlab>=4.4.0",
+        "tree-sitter-languages>=1.10.2",
+        "tree-sitter>=0.21.3",
+        "python-dotenv>=1.0.1",
     ],
     entry_points={
         "console_scripts": [
