5c1bcb3675e94e7320c850961d322ff6a41b7bb7
Stanislav <goncharuk.sm@yandex.ru>
2025-02-20 20:57:41 +0300

    update cpp query

--- repomap/queries/tree-sitter-cpp-tags.scm
+++ repomap/queries/tree-sitter-cpp-tags.scm
@@ -1,15 +1,25 @@
-(struct_specifier name: (type_identifier) @name.definition.class body:(_)) @definition.class
+(struct_specifier
+  name: (type_identifier) @name_definition_class
+  body: (_)) @definition_class
 
-(declaration type: (union_specifier name: (type_identifier) @name.definition.class)) @definition.class
+(declaration
+  type: (union_specifier
+    name: (type_identifier) @name_definition_class)) @definition_class
 
-(function_declarator declarator: (identifier) @name.definition.function) @definition.function
+(function_declarator
+  declarator: (identifier) @name_definition_function) @definition_function
 
-(function_declarator declarator: (field_identifier) @name.definition.function) @definition.function
+(function_declarator
+  declarator: (field_identifier) @name_definition_function) @definition_function
 
-(function_declarator declarator: (qualified_identifier scope: (namespace_identifier) @scope name: (identifier) @name.definition.method)) @definition.method
+(function_declarator
+  declarator: (qualified_identifier) @name_definition_method) @definition_method
 
-(type_definition declarator: (type_identifier) @name.definition.type) @definition.type
+(type_definition
+  declarator: (type_identifier) @name_definition_type) @definition_type
 
-(enum_specifier name: (type_identifier) @name.definition.type) @definition.type
+(enum_specifier
+  name: (type_identifier) @name_definition_type) @definition_type
 
-(class_specifier name: (type_identifier) @name.definition.class) @definition.class
+(class_specifier
+  name: (type_identifier) @name_definition_class) @definition_class
